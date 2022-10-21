31a86da06042f0ee166b5b0a216e6b1e243990d9
Nyberg988 <carl.nyberg@hotmail.com>
2022-10-21 03:00:45 +0000

    061 sending real emails

--- aibel_watches/settings.py
+++ aibel_watches/settings.py
@@ -212,7 +212,7 @@ DEFAULT_FROM_EMAIL = 'aibelwatches@example.com'
 
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'boutiqueado@example.com'
+    DEFAULT_FROM_EMAIL = 'aibelwatches@example.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
